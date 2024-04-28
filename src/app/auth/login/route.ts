12dbc11aa9793322d01c89375a481f93eace9032
Viraj Agharkar <viraj@quolam.com>
2024-04-29 00:50:25 +0530

    Fixes removed unwanted type

--- src/app/auth/login/route.ts
+++ src/app/auth/login/route.ts
@@ -2,15 +2,13 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-import type { Auth } from '@/lib/database.types'
-
 export async function POST(request: Request) {
   const requestUrl = new URL(request.url)
   const formData = await request.formData()
   const email = String(formData.get('email'))
   const password = String(formData.get('password'))
   const cookieStore = cookies()
-  const supabase = createRouteHandlerClient<Auth>({ cookies: () => cookieStore })
+  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
 
   await supabase.auth.signInWithPassword({
     email,
